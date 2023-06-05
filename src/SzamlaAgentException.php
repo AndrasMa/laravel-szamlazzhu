1e99eb19f4e11f06d684e7673b6f2498cc885a79
Bornemisza Sándor Gergely <bornemisza@omisai.com>
2023-06-05 16:19:02 +0200

    SzamlaAgentException more type hinted

--- src/SzamlaAgentException.php
+++ src/SzamlaAgentException.php
@@ -2,68 +2,63 @@
 
 namespace Omisai\SzamlazzhuAgent;
 
-/**
- * Számla Agent egyedi kivételeket kezelő osztály
- */
 class SzamlaAgentException extends \Exception
 {
-    const SYSTEM_DOWN = 'Az oldal jelenleg karbantartás alatt áll. Kérjük, látogass vissza pár perc múlva.';
+    public const SYSTEM_DOWN = 'Az oldal jelenleg karbantartás alatt áll. Kérjük, látogass vissza pár perc múlva.';
 
-    const REQUEST_TYPE_NOT_EXISTS = 'A kérés típusa nem létezik';
+    public const REQUEST_TYPE_NOT_EXISTS = 'A kérés típusa nem létezik';
 
-    const RESPONSE_TYPE_NOT_EXISTS = 'A válasz típusa nem létezik';
+    public const RESPONSE_TYPE_NOT_EXISTS = 'A válasz típusa nem létezik';
 
-    const XML_SCHEMA_TYPE_NOT_EXISTS = 'Az XML séma típusa nem létezik';
+    public const XML_SCHEMA_TYPE_NOT_EXISTS = 'Az XML séma típusa nem létezik';
 
-    const XML_KEY_NOT_EXISTS = 'XML kulcs nem létezik';
+    public const XML_KEY_NOT_EXISTS = 'XML kulcs nem létezik';
 
-    const XML_NOT_VALID = 'Az összeállított XML nem érvényes';
+    public const XML_NOT_VALID = 'Az összeállított XML nem érvényes';
 
-    const XML_DATA_NOT_AVAILABLE = 'Hiba történt az XML adatok összeállításánál: nincs adat.';
+    public const XML_DATA_NOT_AVAILABLE = 'Hiba történt az XML adatok összeállításánál: nincs adat.';
 
-    const XML_DATA_BUILD_FAILED = 'Az XML adatok összeállítása sikertelen';
+    public const XML_DATA_BUILD_FAILED = 'Az XML adatok összeállítása sikertelen';
 
-    const FIELDS_CHECK_ERROR = 'Hiba a mezők ellenőrzése közben';
+    public const FIELDS_CHECK_ERROR = 'Hiba a mezők ellenőrzése közben';
 
-    const DATE_FORMAT_NOT_EXISTS = 'Nincs ilyen dátum formátum';
+    public const DATE_FORMAT_NOT_EXISTS = 'Nincs ilyen dátum formátum';
 
-    const NO_AGENT_INSTANCE_WITH_USERNAME = 'Nincs ilyen felhasználónévvel Számla Agent példányosítva!';
+    public const NO_AGENT_INSTANCE_WITH_USERNAME = 'Nincs ilyen felhasználónévvel Számla Agent példányosítva!';
 
-    const NO_AGENT_INSTANCE_WITH_APIKEY = 'Nincs ilyen kulccsal Számla Agent példányosítva!';
+    public const NO_AGENT_INSTANCE_WITH_APIKEY = 'Nincs ilyen kulccsal Számla Agent példányosítva!';
 
-    const NO_SZLAHU_KEY_IN_HEADER = 'Érvénytelen válasz!';
+    public const NO_SZLAHU_KEY_IN_HEADER = 'Érvénytelen válasz!';
 
-    const DOCUMENT_DATA_IS_MISSING = 'A bizonylat PDF adatai hiányoznak!';
+    public const DOCUMENT_DATA_IS_MISSING = 'A bizonylat PDF adatai hiányoznak!';
 
-    const PDF_FILE_SAVE_SUCCESS = 'PDF fájl mentése sikeres';
+    public const PDF_FILE_SAVE_SUCCESS = 'PDF fájl mentése sikeres';
 
-    const PDF_FILE_SAVE_FAILED = 'PDF fájl mentése sikertelen';
+    public const PDF_FILE_SAVE_FAILED = 'PDF fájl mentése sikertelen';
 
-    const AGENT_RESPONSE_NO_CONTENT = 'A Számla Agent válaszában nincs tartalom!';
+    public const AGENT_RESPONSE_NO_CONTENT = 'A Számla Agent válaszában nincs tartalom!';
 
-    const AGENT_RESPONSE_NO_HEADER = 'A Számla Agent válasza nem tartalmaz fejlécet!';
+    public const AGENT_RESPONSE_NO_HEADER = 'A Számla Agent válasza nem tartalmaz fejlécet!';
 
-    const AGENT_RESPONSE_IS_EMPTY = 'A Számla Agent válasza nem lehet üres!';
+    public const AGENT_RESPONSE_IS_EMPTY = 'A Számla Agent válasza nem lehet üres!';
 
-    const AGENT_ERROR = 'Agent hiba';
+    public const AGENT_ERROR = 'Agent hiba';
 
-    const FILE_CREATION_FAILED = 'A fájl létrehozása sikertelen.';
+    public const FILE_CREATION_FAILED = 'A fájl létrehozása sikertelen.';
 
-    const ATTACHMENT_NOT_EXISTS = 'A csatolandó fájl nem létezik';
+    public const ATTACHMENT_NOT_EXISTS = 'A csatolandó fájl nem létezik';
 
-    const INVOICE_NOTIFICATION_SEND_FAILED = 'Számlaértesítő kézbesítése sikertelen';
+    public const INVOICE_NOTIFICATION_SEND_FAILED = 'Számlaértesítő kézbesítése sikertelen';
 
-    const INVALID_JSON = 'Érvénytelen JSON';
+    public const INVALID_JSON = 'Érvénytelen JSON';
 
-    const INVOICE_EXTERNAL_ID_IS_EMPTY = 'A külső számlaazonosító üres';
+    public const INVOICE_EXTERNAL_ID_IS_EMPTY = 'A külső számlaazonosító üres';
 
-    const CONNECTION_ERROR = 'Sikertelen kapcsolódás';
+    public const CONNECTION_ERROR = 'Sikertelen kapcsolódás';
 
-    const XML_FILE_SAVE_FAILED = 'XML fálj mentése sikertelen';
+    public const XML_FILE_SAVE_FAILED = 'XML fálj mentése sikertelen';
 
     /**
-     * Számla Agent egyedi kivétel létrehozása
-     *
      * @param  string  $message
      * @param  int  $code
      * @param  \Exception  $previous
