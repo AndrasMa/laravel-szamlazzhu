a2683d52b9e3cf5b6326334155ac424a0e7aa254
Bornemisza Sándor Gergely <bornemisza@omisai.com>
2023-06-05 16:25:22 +0200

    Test environment added

---
+++ tests/TestCase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Omisai\SzamlazzhuAgent\Tests;
+
+use Omisai\SzamlazzhuAgent\SzamlaAgentServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+abstract class TestCase extends OrchestraTestCase
+{
+    protected $loadEnvironmentVariables = true;
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            SzamlaAgentServiceProvider::class,
+        ];
+    }
+    /**
+     * Resolve application core environment variables implementation.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function resolveApplicationEnvironmentVariables($app)
+    {
+        if (property_exists($this, 'loadEnvironmentVariables') && $this->loadEnvironmentVariables === true) {
+            $app->useEnvironmentPath(__DIR__ . '/..');
+            $app->make('Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables')->bootstrap($app);
+        }
+    }
+}
