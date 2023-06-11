7d59f21578da1d72e40c838b7cb27d3e0f82c464
Bornemisza Sándor Gergely <bornemisza@omisai.com>
2023-06-11 20:59:38 +0200

    ReverseReceiptHeader code cleared up

--- src/Header/ReverseReceiptHeader.php
+++ src/Header/ReverseReceiptHeader.php
@@ -15,23 +15,4 @@ class ReverseReceiptHeader extends ReceiptHeader
         parent::__construct($receiptNumber);
         $this->setType(Type::REVERSE_RECEIPT);
     }
-
-    /**
-     * @throws SzamlaAgentException
-     */
-    public function checkField($field, $value): mixed
-    {
-        if (property_exists(get_parent_class($this), $field) || property_exists($this, $field)) {
-            $required = in_array($field, $this->requiredFields);
-            switch ($field) {
-                case 'receiptNumber':
-                case 'pdfTemplate':
-                case 'callId':
-                    SzamlaAgentUtil::checkStrField($field, $value, $required, __CLASS__);
-                    break;
-            }
-        }
-
-        return $value;
-    }
 }
