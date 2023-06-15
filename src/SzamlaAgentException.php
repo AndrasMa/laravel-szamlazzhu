262e6d1be6ec778c4f7e6225240c72340d9befcd
Bornemisza Sándor Gergely <bornemisza@omisai.com>
2023-06-15 10:10:00 +0200

    Translate SzamlaAgentException error messages to English

--- src/SzamlaAgentException.php
+++ src/SzamlaAgentException.php
@@ -4,62 +4,60 @@ namespace Omisai\Szamlazzhu;
 
 class SzamlaAgentException extends \Exception
 {
-    public const SYSTEM_DOWN = 'Az oldal jelenleg karbantartás alatt áll. Kérjük, látogass vissza pár perc múlva.';
+    public const SYSTEM_DOWN = 'The site is currently under maintenance. Please come back in a few minutes.';
 
-    public const REQUEST_TYPE_NOT_EXISTS = 'A kérés típusa nem létezik';
+    public const REQUEST_TYPE_NOT_EXISTS = 'The request type does not exist';
 
-    public const RESPONSE_TYPE_NOT_EXISTS = 'A válasz típusa nem létezik';
+    public const RESPONSE_TYPE_NOT_EXISTS = 'The response type does not exist';
 
-    public const XML_SCHEMA_TYPE_NOT_EXISTS = 'Az XML séma típusa nem létezik';
+    public const XML_SCHEMA_TYPE_NOT_EXISTS = 'The XML schema type does not exist';
 
-    public const XML_KEY_NOT_EXISTS = 'XML kulcs nem létezik';
+    public const XML_KEY_NOT_EXISTS = 'XML key does not exist';
 
-    public const XML_NOT_VALID = 'Az összeállított XML nem érvényes';
+    public const XML_NOT_VALID = 'The assembled XML is not valid';
 
-    public const XML_DATA_NOT_AVAILABLE = 'Hiba történt az XML adatok összeállításánál: nincs adat.';
+    public const XML_DATA_NOT_AVAILABLE = 'An error occurred while assembling XML data: no data available.';
 
-    public const XML_DATA_BUILD_FAILED = 'Az XML adatok összeállítása sikertelen';
+    public const XML_DATA_BUILD_FAILED = 'Failed to build XML data';
 
-    public const FIELDS_CHECK_ERROR = 'Hiba a mezők ellenőrzése közben';
+    public const FIELDS_CHECK_ERROR = 'Error during field validation';
 
-    public const DATE_FORMAT_NOT_EXISTS = 'Nincs ilyen dátum formátum';
+    public const DATE_FORMAT_NOT_EXISTS = 'Date format does not exist';
 
-    public const NO_AGENT_INSTANCE_WITH_USERNAME = 'Nincs ilyen felhasználónévvel Számla Agent példányosítva!';
+    public const NO_AGENT_INSTANCE_WITH_USERNAME = 'No Invoice Agent instance instantiated with this username!';
 
-    public const NO_AGENT_INSTANCE_WITH_APIKEY = 'Nincs ilyen kulccsal Számla Agent példányosítva!';
+    public const NO_AGENT_INSTANCE_WITH_APIKEY = 'No Invoice Agent instance instantiated with this API key!';
 
-    public const NO_SZLAHU_KEY_IN_HEADER = 'Érvénytelen válasz!';
+    public const NO_SZLAHU_KEY_IN_HEADER = 'Invalid response!';
 
-    public const DOCUMENT_DATA_IS_MISSING = 'A bizonylat PDF adatai hiányoznak!';
+    public const DOCUMENT_DATA_IS_MISSING = 'Invoice PDF data is missing!';
 
-    public const PDF_FILE_SAVE_SUCCESS = 'PDF fájl mentése sikeres';
+    public const PDF_FILE_SAVE_SUCCESS = 'PDF file saved successfully';
 
-    public const PDF_FILE_SAVE_FAILED = 'PDF fájl mentése sikertelen';
+    public const PDF_FILE_SAVE_FAILED = 'Failed to save PDF file';
 
-    public const AGENT_RESPONSE_NO_CONTENT = 'A Számla Agent válaszában nincs tartalom!';
+    public const AGENT_RESPONSE_NO_CONTENT = 'The Invoice Agent response has no content!';
 
-    public const AGENT_RESPONSE_NO_HEADER = 'A Számla Agent válasza nem tartalmaz fejlécet!';
+    public const AGENT_RESPONSE_NO_HEADER = 'The Invoice Agent response does not contain a header!';
 
-    public const AGENT_RESPONSE_IS_EMPTY = 'A Számla Agent válasza nem lehet üres!';
+    public const AGENT_RESPONSE_IS_EMPTY = 'The Invoice Agent response cannot be empty!';
 
-    public const AGENT_ERROR = 'Agent hiba';
+    public const AGENT_ERROR = 'Agent error';
 
-    public const FILE_CREATION_FAILED = 'A fájl létrehozása sikertelen.';
+    public const FILE_CREATION_FAILED = 'Failed to create file.';
 
-    public const ATTACHMENT_NOT_EXISTS = 'A csatolandó fájl nem létezik';
+    public const INVOICE_NOTIFICATION_SEND_FAILED = 'Failed to deliver invoice notification';
 
-    public const INVOICE_NOTIFICATION_SEND_FAILED = 'Számlaértesítő kézbesítése sikertelen';
+    public const INVALID_JSON = 'Invalid JSON';
 
-    public const INVALID_JSON = 'Érvénytelen JSON';
+    public const INVOICE_EXTERNAL_ID_IS_EMPTY = 'The external invoice ID is empty';
 
-    public const INVOICE_EXTERNAL_ID_IS_EMPTY = 'A külső számlaazonosító üres';
+    public const CONNECTION_ERROR = 'Connection failed';
 
-    public const CONNECTION_ERROR = 'Sikertelen kapcsolódás';
-
-    public const XML_FILE_SAVE_FAILED = 'XML fálj mentése sikertelen';
+    public const XML_FILE_SAVE_FAILED = 'Failed to save XML file';
 
     public function __toString()
     {
-        return __CLASS__.": [{$this->code}]: {$this->message}\n";
+        return self::class . ": [{$this->code}]: {$this->message}\n";
     }
 }
